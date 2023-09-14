7fa595b0530cb01610630876c3019090e85fcbcd
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-14 01:07:41 +0000

    rust is too fun man!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+const NUM: u32 = 100_000;
+
 fn main() {
-    println!("Hello, world!");
+    // Undersctanding Immutability
+    {
+        let mut x = 5;
+        println!("The value of x is: {}", x);
+        x = 6;
+        println!("The value of x now is: {}", x);
+        let z;
+        z = x + NUM;
+        println!("The constant + x is: {}", z);
+    }
+
+    //Shadowing
+    {
+        let x = 5;
+        println!("The value of x is: {}", x);
+        let x = 5 + 1;
+        println!("The value of x here is: {}", x);
+        let x = 5 + 2;
+        println!("The value of x here is: {}", x);
+    }
+
+    //Tuples
+    {
+        let tup = (23, -32, 0.56);
+
+        let (x, y, z) = tup;
+
+        println!("The tuple values are: {}",tup);
+    }
 }
