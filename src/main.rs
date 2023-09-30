c0a922c1d5c4c1551aa249f41aef7090f5237f61
kekeli-the-light <kekelidompeh@gmail.com>
2023-09-30 21:51:22 +0000

    learnt about enums

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
+#[derive(Debug)]
 enum Payment {
     cash,
-    creditcard
+    creditcard,
+}
+
+#[derive(Debug)]
+struct Pay1 {
+    name: String,
+    payment: Payment
 }
 
 fn main() {
     //ENUMERATIONS
-    let some_payment = Payment::cash;
-    let another_payment = Payment::creditcard;
+    let person1 = Pay1{
+        name: String::from("Mike"),
+        payment: Payment::cash
+    };
 
-
+    println!("{:?}", person1);
 }
 
+
+
+// fn add(card: Payment, money: Payment){
+//
+// }
 
