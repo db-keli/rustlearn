8ed19eba20fad542d32208476697c022f96c3b5b
kekeli-the-light <kekelidompeh@gmail.com>
2023-10-02 22:41:40 +0000

    structs and enums

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,30 @@ enum Payment {
     creditcard,
 }
 
+enum CarType {
+    Hatch,
+    Sudan,
+    SUV
+}
+
 #[derive(Debug)]
 struct Pay1 {
     name: String,
     payment: Payment
+}
+
+#[derive(Debug)]
+enum Avatar {
+    Fire,
+    Air,
+    Water,
+    Earth,
+}
+
+#[derive(Debug)]
+struct Cast {
+    name: String,
+    nation: Avatar,
 }
 
 fn main() {
@@ -18,7 +38,32 @@ fn main() {
     };
 
     println!("{:?}", person1);
+
+    print_size(CarType::SUV);
+
+    let person2 = Cast{
+        name: String::from("Aang"),
+        nation: Avatar::Air
+    };
+    println!("{:?}", person2);
 }
+
+fn print_size(car:CarType) {
+    match car {
+        CarType::Hatch => {
+            println!("Small size car");
+        },
+        CarType::Sudan => {
+            println!("Medium sized car");
+        },
+        CarType::SUV => {
+            println!("Large sized sports utility car");
+        }
+
+    }
+}
+
+
 
 
 
